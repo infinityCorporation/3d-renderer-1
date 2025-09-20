84ce97ef8fbe8a45ed5d341add7d2c23e335e08e
Evan Frisbie <ef@Evans-MacBook-Pro.local>
2025-09-20 11:36:34 -0400

    init commit

---
+++ build/CMakeFiles/render.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/render.dir/main.cc.o"
+  "CMakeFiles/render.dir/main.cc.o.d"
+  "render"
+  "render.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/render.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
